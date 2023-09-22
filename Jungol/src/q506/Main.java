11d2dc725aef40d96e12e35efdb6c2ee2e998f31
19-3rsys-004g <19-3rsys-004g@19-3rsys-004>
2023-09-22 17:44:36 +0900

    506

---
+++ Jungol/src/q506/Main.java
@@ -0,0 +1,13 @@
+package q506;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("My height");
+		int height = 170;
+		System.out.println(height);
+		String w = "My weight";
+		System.out.println(w);
+		double d = 68.6;
+		System.out.printf("%.6f\n", d);
+	}
+}
