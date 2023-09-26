fc14f468966f98d3ec69f465fb97d856ad021e28
19-3rsys-004g <19-3rsys-004g@19-3rsys-004>
2023-09-26 13:48:49 +0900

    632

---
+++ Jungol/src/q105/Main.java
@@ -0,0 +1,12 @@
+package q105;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.printf("%15s%15s%15s\n", "Seoul", "10,312,545", "+91,375");
+		System.out.printf("%15s%15s%15s\n", "Pusan", "3,567,910", "+5,868");
+		System.out.printf("%15s%15s%15s\n", "", "", "");
+		System.out.printf("%15s%15s%15s\n", "", "", "");
+
+	}
+
+}
