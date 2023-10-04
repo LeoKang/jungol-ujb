7c712f8e57704f614196e3e8fbb8d18f41cfad28
19-3rsys-004g <19-3rsys-004g@19-3rsys-004>
2023-10-04 12:29:33 +0900

    127

---
+++ Jungol/src/q127/Main.java
@@ -0,0 +1,24 @@
+package q127;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int sum = 0, cnt = 0;
+		
+		while (true) {
+			int inp = sc.nextInt();
+
+			if (0 > inp || inp > 100) {
+				break;
+			}
+			cnt++;
+			sum += inp;
+		}
+		sc.close();
+		
+		System.out.println("sum : " + sum);
+		System.out.printf("avg : %.1f\n", (double)sum / cnt);
+	}
+}
