a6c993538d6f6237839aadf5d55cc9d049047997
19-3rsys-004g <19-3rsys-004g@19-3rsys-004>
2023-10-05 09:51:29 +0900

    518

---
+++ Jungol/src/q518/Main.java
@@ -0,0 +1,26 @@
+package q518;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int[] inp = new int[3];
+		for (int i = 0; i < inp.length; i++) {
+			inp[i] = sc.nextInt();
+		}
+		sc.close();
+
+//		for (int i = 0; i < inp.length; i++) {
+//			System.out.print(inp[i] + " ");
+//		}
+
+		int sum = 0;
+		for (int i = 0; i < inp.length; i++) {
+			sum += inp[i];
+		}
+		System.out.println("sum : " + sum);
+		int avg = sum / 3;
+		System.out.println("avg : " + avg);
+	}
+}
