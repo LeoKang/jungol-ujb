473cae226794a28702628d416569a0cf1b7e258a
19-3rsys-004g <19-3rsys-004g@19-3rsys-004>
2023-10-17 15:07:29 +0900

    Oracle3

--- Jungol/src/oracle3/MemberTest.java
+++ Jungol/src/oracle3/MemberTest.java
@@ -1,11 +1,18 @@
 package oracle3;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class MemberTest {
 	public static void main(String[] args) {
+		System.out.print("Input ename...? ");
+		Scanner sc = new Scanner(System.in);
+		String inp = sc.next();
+		System.out.println(inp);
+		sc.close();
+		
 		MemberDAO dao = new MemberDAO();
-		ArrayList<MemberVo> list = dao.list();
+		ArrayList<MemberVo> list = dao.list(inp);
 
 		for (int i = 0; i < list.size(); i++) {
 			MemberVo data = (MemberVo) list.get(i);
