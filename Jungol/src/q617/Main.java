8a300e2c2a3c600f846d771a28469c099ab83f0e
19-3rsys-004g <19-3rsys-004g@19-3rsys-004>
2023-10-24 14:43:44 +0900

    617

---
+++ Jungol/src/q617/Main.java
@@ -0,0 +1,50 @@
+package q617;
+
+import java.util.Scanner;
+
+class Person {
+	private String name;
+	private int height;
+
+	public Person(String name, int height) {
+		this.name = name;
+		this.height = height;
+	}
+
+	public void print() {
+		System.out.println(name + " " + height);
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+}
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		Person[] p = new Person[5];
+		for (int i = 0; i < p.length; i++) {
+			String name = sc.next();
+			int height = sc.nextInt();
+			p[i] = new Person(name, height);
+		}
+		sc.close();
+
+		for (int i = 0; i < p.length; i++) {
+			p[i].print();
+		}
+
+		Person min = new Person("", 200);
+		for (int i = 0; i < p.length; i++) {
+			if (min.getHeight() > p[i].getHeight()) {
+				min = p[i];
+			}
+		}
+		min.print();
+	}
+}
